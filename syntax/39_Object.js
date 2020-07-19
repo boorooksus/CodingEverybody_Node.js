641047872fcc9d56ff69ea5b1c8f3ae1bf7ba207
boorooksus <boorooksus@gmail.com>
2020-07-19 21:53:59 +0900

    OOP concept

---
+++ syntax/39_Object.js
@@ -0,0 +1,14 @@
+//Array vs OOP:
+//배열은 순서O, 객체는 순서X
+
+//배열
+var members = ['abc', 'def', 'hij'];
+console.log(members[1]); // abc
+
+//객체
+var roles = {
+    'name':'abc',
+    'job':'student',
+    'grade':'a'
+}
+console.log(roles.name); // abc
